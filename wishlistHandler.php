9906809f47c0470be02f28fa2bf2db15d4fb94df
Brayan-Maldonado <bmaldonado574@gmail.com>
2024-04-23 21:54:49 -0400

    switch in wishlistHandler

--- wishlistHandler.php
+++ wishlistHandler.php
@@ -21,20 +21,37 @@ if (isset($_SESSION['username'])) {
     // Retrieve property name from the form
     $propertyName = $_POST['property_name'];
 
+    switch($propertyName){
+        case 'phineas':
+            $propertyName='Phineas';
+            break;
+        case 'simpsons':
+            $propertyName='Simpsons';
+            break;
+        case 'spongebob':
+            $propertyName='Spongebob';
+            break;
+        case 'familyguy':
+            $propertyName='FamilyGuy';
+            break;
+        case 'gravityfall':
+            $propertyName='Gravity Falls';
+            break;
+        case 'teentitans':
+            $propertyName='Teen Titans';
+            break;
+    }
+
     // Get property ID from database
     $sql = "SELECT id FROM properties WHERE name = '$propertyName'";
     $result = $conn->query($sql);
-    echo '<pre>';
-    var_dump($_SESSION);
-    var_dump($_POST);
-    echo '</pre>';
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $propertyId = $row['id'];
 
         // Add property to wishlist
-        $sql = "INSERT INTO wishlist (user_id, username, property_id, property_name) VALUES ('$username', '$propertyName', '$propertyId')";
+        $sql = "INSERT INTO wishlist (user_id, property_id) VALUES (user_id, (SELECT id FROM properties WHERE name = $propertyName))";
         if ($conn->query($sql) === TRUE) {
             echo "Property added to wishlist successfully.";
         } else {
